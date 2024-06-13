0e5c529a4ee704dc3258015c65538a34fb814f0c
Sydney <sydney@skyedelaciel.com>
2024-06-12 20:03:11 -0700

    use TR instead of shell substitution

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 fileName=$1
 outName=${fileName%.*}
-#remove spaces
-outName="${outName// /_}"
-#remove spaces
+#remove spaces (can't use params cause cloudflare has a conniption)
+outName=$(echo "$outName" | tr ' ' '_' )
+#remove docs
 outName="${outName#"docs/"}"
 
 shift
